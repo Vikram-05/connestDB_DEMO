dc3e8fa773a2b59a36cb5f0a5ba9ece6bab604eb
Vikram-05 <vikramdbs9097@gmail.com>
2026-04-20 14:41:07 +0530

    connected

---
+++ server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+
+import {connectDB} from './database/connection.js';    
+import dotenv from 'dotenv'
+dotenv.config()
+
+const PORT = process.env.PORT || 3000;
+
+const app =express();
+
+
+
+app.get('/',(req,res) => {
+    res.status(200).json({message: 'Hello World!'});
+})
+
+
+app.listen(PORT,async()=>{
+    await connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
